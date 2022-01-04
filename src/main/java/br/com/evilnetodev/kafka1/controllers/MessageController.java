84af402b512ec37ab9b38bb7eb28f5d68c8d5f94
EvillNeto <evilasio_neto12@hotmail.com>
2022-01-04 17:15:06 -0300

    passado a logicado par o pacote de service atraves da criação das classe de envio e recebimento(não implementado) de mensagens

--- src/main/java/br/com/evilnetodev/kafka1/controllers/MessageController.java
+++ src/main/java/br/com/evilnetodev/kafka1/controllers/MessageController.java
@@ -7,37 +7,24 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.services.KafkaSender;
+
 @RestController
 @RequestMapping("/message")
 public class MessageController {
 
+    @Autowired
+    private KafkaSender kafkaSender;
+
     @GetMapping("/{message}")
-    public ResponseEntity<Object> sendMenssage(@PathVariable(name = "message") String message) throws InterruptedException, ExecutionException{
-        var producer =new KafkaProducer<String,String>(properties());
-        var record = new ProducerRecord<>("TESTE_MENSAGEM_1", message, message);
-        producer.send(record, (data, ex)-> {
-            if(ex != null){
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("sucesso enviando" + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
-        }).get();
-        
-        return null;
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
+    public String sendMenssage(@PathVariable(name = "message") String message) throws InterruptedException, ExecutionException{
+        return kafkaSender.send(message);
     }
 }
